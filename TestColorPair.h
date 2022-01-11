5bda6ad1eecfcb6bf1619d2e6c7a02c80fde6014
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-01-11 22:25:34 +0530

    Update TestColorPair.h

--- TestColorPair.h
+++ TestColorPair.h
@@ -1,4 +1,2 @@
-#include "ColorPairDefines.h"
-
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
